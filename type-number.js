571d8628b4fbba13fad43924ce5f139549c2992d
anna julia de lima <160244526+4nn4julia@users.noreply.github.com>
2024-11-22 08:54:18 -0300

    carga_js-tp-var-fn

---
+++ type-number.js
@@ -0,0 +1,24 @@
+//tipo Number
+
+const meuNumero=3;
+
+const primeiroNumero=1;
+const segundoNumero=2;
+
+const operacaoMatematica=primeiroNumero-segundoNumero;
+
+console.log(operacaoMatematica)
+
+//ponto flutuante
+
+const numeroPontoflutuando=3.3;
+const pontoFlutuanteSemZero=.5;
+
+const novaOperacao=primeiroNumero/numeroPontoflutuando;
+
+console.log(novaOperacao)
+
+//NaN->Not A Number(nao e um número)
+
+const alura="Alura";
+console.log(alura*primeiroNumero)
